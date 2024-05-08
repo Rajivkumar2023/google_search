5ff1caa7708ae57336de562ad32573c8f879fe7a
Rajiv kumar <rajivdhanama00@gmail.com>
2024-05-08 16:59:32 +0530

    some changes

--- scripts.js
+++ scripts.js
@@ -1,20 +1,10 @@
-function search() {
-    // Get the search query from the input field
+function search() { 
     var searchQuery = document.getElementById("search-input").value;
-    
-    // Perform the search (for demonstration purposes, just adding a few example results)
     var newResults = [
-        "'" + searchQuery + "'",
+        `${searchQuery}`,
     ];
-
-    // Add the new results to the search results list
     var searchResultsList = document.getElementById("search-results-list");
-    newResults.forEach(function(resultText) {
-        var listItem = document.createElement("li");
-        var link = document.createElement("a");
-        link.setAttribute("href", "#");
-        link.textContent = resultText;
-        listItem.appendChild(link);
-        searchResultsList.appendChild(listItem);
+    newResults.forEach(resultText => {
+        searchResultsList.innerHTML += `<li><a href="#">${resultText}</a></li>`;
     });
 }
